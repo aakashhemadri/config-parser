d79d711a34ba0a2ad96054812438bc423600ac45
Aakash Hemadri <aakashhemadri123@gmail.com>
2020-02-05 14:03:58 +0530

    final

--- include/yacp.h
+++ include/yacp.h
@@ -5,7 +5,7 @@ namespace yacp
 {
 struct yacp_struct
 {
-    std::unordered_map<std::string, std::pair<std::string, std::string>> SET;
+    std::unordered_map<std::string, std::string> SET;
     std::unordered_map<std::string, std::pair<std::string, std::string>> RESOURCE;
 
 } yacp;
